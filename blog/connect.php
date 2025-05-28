98e21da353329817ad58f4588f035be4f82c0d55
EmelineDe <delobel.emeline@gmail.com>
2025-05-28 14:55:01 +0200

    Préparation pour déploiement Render

--- blog/connect.php
+++ blog/connect.php
@@ -1,9 +1,13 @@
 <?php
-
-
 try {
-    $opt =[PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION];
-    $db = @new PDO('mysql:host=localhost;dbname=travel_blog; charset=utf8','root', 'root', $opt);
-} catch (PDOException $pdoe) {
-    die('Impossible de se connecter.Err : ' .$pdoe->getMessage());
+    $opt = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION];
+
+    $host = getenv('DB_HOST');
+    $dbname = getenv('DB_NAME');
+    $user = getenv('DB_USER');
+    $pass = getenv('DB_PASS');
+
+    $db = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass, $opt);
+} catch (PDOException $e) {
+    die('Erreur de connexion à la base : ' . $e->getMessage());
 }
